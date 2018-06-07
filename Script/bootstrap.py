c82ab9953fc2d8485e149ffb01b9d74e32a7e796
Смирнов Вадим Павлович <va.smirnov@2gis.ru>
2018-06-08 00:31:08 +0700

    Add bootstrap

---
+++ Script/bootstrap.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import logging
+import argparse
+import subprocess
+
+logging.basicConfig(level=logging.INFO,
+					format="%(asctime)s %(levelname)s %(message)s")
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+parse_dir = os.path.join(script_dir, "days")
+diff_dir = os.path.join(script_dir, "diff")
+
+def main():
+	parser = create_parser()
+	args = parser.parse_args()
+
+	logging.debug("Passed arguments: {}".format(args))
+	install_packages()
+
+def create_parser():
+	parser = argparse.ArgumentParser(description="Generating v4iOS module..")
+	parser.add_argument("--name", "-n",
+		help="Module name",
+		default="")
+	return parser
+
+def install_packages():
+	subprocess.check_call(["swift", "build"]) 
+
+if __name__ == "__main__":
+	main()
